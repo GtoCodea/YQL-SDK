0493a48c0e81e1367cbfe8ec756968d54742dcc5
Isidro Aguilar <isidro.aguilar94@hotmail.com>
2016-06-24 01:58:39 -0500

    Add files via upload
    YQL SDK

---
+++ ApiDefinition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using UIKit;
+using Foundation;
+using ObjCRuntime;
+using CoreGraphics;
+
+namespace YQLSDK
+{
+	interface IYQL {}
+
+	[BaseType (typeof(NSObject))]
+	interface YQL{
+		[Export("query:")]
+		NSDictionary Query (string query);
+	}
+}
+
---
+++ libYQL.linkwith.cs
@@ -0,0 +1,3 @@
+using ObjCRuntime;
+
+[assembly: LinkWith ("libYQL.a", SmartLink = true, ForceLoad = true)]
